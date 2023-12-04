61ac559c0304c7ac0c2ea7c6e8f29098d3ff8e2a
sartrey <sartrey@163.com>
2023-12-04 23:09:34 +0800

    support seperated client & server & script tsconfig and npm commands

---
+++ src/script/start-client.ts
@@ -0,0 +1,21 @@
+import path from 'path';
+
+import { IMaybeAppConfig } from '@epiijs/config';
+import { startClient } from '@epiijs/client';
+
+const config: IMaybeAppConfig = {
+  root: path.join(__dirname, '../../'),
+  dirs: {
+    client: 'client',
+    server: 'server'
+  },
+  port: 4001
+}
+
+// 如果 server 需要和 client 混用，需要在 server 自行由业务实现 StaticFiles handler
+startClient(config);
+
+// TODO: 判定 phase 或者直接从 cli 识别参数判定是否是 构建指令
+// if (process.env.epii_phase === 'build') {
+//   buildClient(config);
+// }
