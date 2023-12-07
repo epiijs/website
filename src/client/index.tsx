c14ae05edc8ccafcc4e2b99eeb356af3bdc35127
sartrey <sartrey@163.com>
2023-12-07 23:46:51 +0800

    try vite

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import './index.less';
 
 export default function Application() {
-  return (
+  const [stateValue, setStateValue] = useState<number>(0);
+
+  const raiseValueAdd = () => {
+    setStateValue(stateValue + 1);
+  };
+
+  return (<div className="container">
     <div>hello world</div>
-  );
+    <div className="actions">
+      <div>{stateValue}</div>
+      <div><button onClick={raiseValueAdd}>add</button></div>
+    </div>
+  </div>);
 }
